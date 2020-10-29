e3c78d100c761a9c255ec5690cffacfc16b63818
dev-tronifier <66989866+dev-tronifier@users.noreply.github.com>
2020-10-29 07:57:35 +0530

    Update test_mod3.c

--- test/test_mod3.c
+++ test/test_mod3.c
@@ -1,12 +1,6 @@
 /**
- * FIXME: Errors while compiling(until I manually copy my header files in
- *	target(/usr/local/include) in my case:
- *
- * In file included from ./test.c:1:0:
- * /usr/local/include/proj_name/module3.h:4:10: fatal error: module3/file00.h: No such file or directory
- * #include "module3/file00.h"
- *          ^~~~~~~~~~~~~~~~~~
- * compilation terminated.
+ * Compile using:
+ * gcc test_mod3.c -o test_mod3 -lmodule3
  */
 
 #include <proj_name/module3.h>
